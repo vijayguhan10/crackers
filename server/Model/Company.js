ab471a6e4eee4ecb4cb257ff2343cc43934e2f8a
vijayguhan10 <vijayguhan10@gmail.com>
2025-01-22 23:39:27 +0530

    updated the code with the resolved conflicts

--- server/Model/Company.js
+++ server/Model/Company.js
@@ -60,18 +60,6 @@ const companySchema = new mongoose.Schema({
       type: String,
       required: true
     }
-  },
-  totalrevenue: {
-    type: Number,
-    default: 0
-  },
-  totalinvoices: {
-    type: Number,
-    default: 0
-  },
-  totalcustomers: {
-    type: Number,
-    default: 0
   }
 });
 
